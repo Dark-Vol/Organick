b18eac84b0fc2250011f0fb320c0d0d4adb1cfde
Dark-Vol <overlord28985@gmail.com>
2024-11-12 23:28:12 +0200

    add database for about

---
+++ src/database/about/offerCatalog.js
@@ -0,0 +1,23 @@
+const offerCatalog = [
+    {
+        id: 1,
+        name: "Spicy",
+        img: "../img/Offer/Spicy-min.jpg",
+    },
+    {
+        id: 2,
+        name: "Nuts & Feesd",
+        img: "../img/Offer/Nuts & Feesd-min.jpg",
+    },
+    {
+        id: 3,
+        name: "Fruits",
+        img: "../img/Offer/Fruits-min.jpg",
+    },
+    {
+        id: 4,
+        name: "Vegatable",
+        img: "../img/Offer/Vegatable-min.jpg",
+    },
+];
+export default offerCatalog;
---
+++ src/database/about/team.js
@@ -0,0 +1,21 @@
+const team = [
+    {
+        id: 1,
+        img: "../img/Team/Giovani Bacardo-min.jpg",
+        name: "Giovani Bacardo",
+        role: "Farmer",
+    },
+    {
+        id: 2,
+        img: "../img/Team/Marianne Loreno-min.jpg",
+        name: "Marianne Loreno",
+        role: "Designer",
+    },
+    {
+        id: 3,
+        img: "../img/Team/Riga Pelore-min.jpg",
+        name: "Riga Pelore",
+        role: "Farmer",
+    },
+];
+export default team;
